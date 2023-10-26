a548ee03a0a26904d55ac4bdda4d6281d7a6bd9a
Andrew Rosen <asrosen93@gmail.com>
2023-10-26 00:14:41 -0700

    patch

--- tests/calculator/test_calc.py
+++ tests/calculator/test_calc.py
@@ -1,7 +1,9 @@
 from xtb_ase.calculator import xTB
 from ase.build import bulk, molecule
 
-def test_molecule():
+def test_molecule_static(tmpdir):
+    tmpdir.chdir()
+    
     atoms = molecule("H2")
     atoms.calc = xTB()
     atoms.get_potential_energy()
@@ -9,7 +11,9 @@ def test_molecule():
     assert atoms.calc.results["forces"].shape == (2, 3)
     assert isinstance(atoms.calc.results["attributes"], dict)
 
-def test_solid():
+def test_solid_static(tmpdir):
+    tmpdir.chdir()
+
     atoms = bulk("Cu")
     atoms.calc = xTB()
     atoms.get_potential_energy()
