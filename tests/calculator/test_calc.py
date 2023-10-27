034c141d6bd0a6f2e4d4af5e9b6eae56a4e5b2b4
Andrew S. Rosen <asrosen93@gmail.com>
2023-10-27 13:11:47 -0700

    Update test_calc.py

--- tests/calculator/test_calc.py
+++ tests/calculator/test_calc.py
@@ -11,14 +11,3 @@ def test_molecule_static(tmpdir):
     assert isinstance(atoms.calc.results["energy"], float)
     assert atoms.calc.results["forces"].shape == (2, 3)
     assert isinstance(atoms.calc.results["attributes"], dict)
-
-
-def test_solid_static(tmpdir):
-    tmpdir.chdir()
-
-    atoms = bulk("Cu")
-    atoms.calc = XTB()
-    atoms.get_potential_energy()
-    assert isinstance(atoms.calc.results["energy"], float)
-    assert atoms.calc.results["forces"].shape == (1, 3)
-    assert isinstance(atoms.calc.results["attributes"], dict)
