01abe7899bcad92df7be82394b354966ecb56a86
Andrew S. Rosen <asrosen93@gmail.com>
2023-10-27 23:45:35 -0700

    Update io.py

--- src/xtb_ase/io.py
+++ src/xtb_ase/io.py
@@ -3,6 +3,7 @@ I/O for xTB
 """
 from __future__ import annotations
 
+import logging
 from pathlib import Path
 from typing import TYPE_CHECKING
 
@@ -80,7 +81,7 @@ def read_xtb(output_filepath: Path | str) -> ccData:
     cclib_obj
         The cclib object containing the xTB results.
     """
-    cclib_obj = ccread(output_filepath)
+    cclib_obj = ccread(output_filepath, logging.ERROR))
 
     if not cclib_obj:
         msg = f"Could not read {output_filepath}"
